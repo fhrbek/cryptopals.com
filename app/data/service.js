8242e42f23e8171ee6bb12038b27bd24a37f5c09
Michal Bryxí <michal.bryxi@gmail.com>
2017-04-10 14:49:53 +0100

    Functional app

--- app/data/service.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Service.extend({
-  sets: [
-    {id: 1, name: 'Convert hex to base64'}
-  ],
-});
--- app/groups/controller.js
+++ app/groups/controller.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  data: Ember.inject.service(),
-
-  // foos: Ember.computed.alias('data.sets'),
-  foos: [
-    {id: 1, name: 'Convert hex to base64'}
-  ],
-  challenges: Ember.computed.alias('sets.challenges')
+  foos: [1,2,3,4],
 });
